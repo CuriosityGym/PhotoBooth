ad4978d308a8fe4b0c5a20b040313b3fae64c7b4
rupin <rupin.chheda@gmail.com>
2017-11-29 16:05:28 +0530

    Deploy

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import os
 from flask import Flask, render_template, request, redirect, session
-
+import paho.mqtt.client as mqtt
+mqttConnected=False
+publishingTotopicName="/CG/photobooth"
 app = Flask(__name__)
 
 
-
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code "+str(rc))
+	#mqttConnected=True
 @app.route('/', methods=['GET'])
 def index():
-       phoneNumber= request.args.get('phoneNumber')
-       message=request.args.get('message')
-       return phoneNumber + " "+ message
+        phoneNumber= request.args.get('phoneNumber')
+        message=request.args.get('message')
+        client = mqtt.Client()
+        client.on_connect = on_connect	
+        client.connect("iot.eclipse.org", 1883, 60)
+        client.loop_start()
+        client.publish(publishingTotopicName, message)        
+        client.loop_stop()
+        return "OK"
 	
     
 
