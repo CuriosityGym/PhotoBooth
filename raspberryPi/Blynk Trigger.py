b0a922776a7946004375af5ec07bf11d1162aae3
rupin <rupin.chheda@gmail.com>
2017-12-15 14:37:16 +0530

    Deploy

---
+++ raspberryPi/Blynk Trigger.py
@@ -0,0 +1,20 @@
+import BlynkLib
+import time
+
+BLYNK_AUTH = 'cdfcfc54ce1d4e7e8d208fda31a2661f'
+
+# Initialize Blynk
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+# Register Virtual Pins
+@blynk.VIRTUAL_WRITE(1)
+def my_write_handler(value):
+    print('Current V1 value: {}'.format(value))
+
+@blynk.VIRTUAL_READ(2)
+def my_read_handler():
+    # this widget will show some time in seconds..
+    blynk.virtual_write(2, time.ticks_ms() // 1000)
+
+# Start Blynk (this call should never return)
+blynk.run()
