4e147bd59361175aa4a74530b68202bc3a24d1bb
rupin <rupin@curiositygym.com>
2018-01-10 13:19:41 +0530

    Before Edits

---
+++ raspberryPi/testBlynk.py
@@ -0,0 +1,18 @@
+import BlynkLib
+
+
+
+BLYNK_AUTH = 'cdfcfc54ce1d4e7e8d208fda31a2661f'
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+# Register Virtual Pins
+@blynk.VIRTUAL_WRITE(1)
+def my_write_handler(value):
+        print('Current V1 value: {}'.format(value))
+        message=value.split(":")
+        recipientNumber=message[0].strip()
+        recipientOTP=message[1].strip()
+        print(recipientOTP)
+
+if __name__ == '__main__':
+	blynk.run()
